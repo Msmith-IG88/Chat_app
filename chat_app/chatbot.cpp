93116aa107709d321da7aaa302bdb664bc844bc2
Michael Smith <Smitmic5@oregonstate.edu>
2023-04-26 12:46:30 -1000

    create chatbot file and edit makefile

---
+++ chat_app/chatbot.cpp
@@ -0,0 +1,106 @@
+#include <iostream>
+#include <string>
+#include <thread>
+#include <future>
+
+#include "tcp.hpp"
+#include "numeric_type_header.hpp"
+
+class chat_client
+{
+private:
+    using connection_ptr = std::shared_ptr<tcp_connection<chat_client>>;
+
+    std::promise<connection_ptr> connection_promise_;
+    std::shared_future<connection_ptr> connection_{connection_promise_.get_future()};
+
+public:
+    // Required by tcp_connection.
+    using header = numeric_type_header<std::size_t>;
+
+    void start_connection(connection_ptr conn)
+    {
+        connection_promise_.set_value(conn);
+    }
+
+    bool read_body(connection_ptr conn, std::vector<char>& message)
+    {
+        char cmd = 
+        std::cout.write(message.data(), message.size());
+        std::cout << '\n';
+        return true;
+    }
+
+    // Required by tcp_connection.
+    void handle_write_error(connection_ptr conn,
+                            const boost::system::error_code& error,
+                            const std::vector<char>& message)
+    {
+    }
+
+    void close_hook(connection_ptr conn, const boost::system::error_code& error)
+    {
+        if (error) {
+            std::cout << "Disconnected from session by host.\n";
+        }
+    }
+
+    bool send(const std::string& message)
+    {
+        auto conn = connection_.get();
+
+        if (!conn) {
+            return false;
+        }
+
+        conn->write(message);
+
+        return true;
+    }
+
+    bool close()
+    {
+        auto conn = connection_.get();
+
+        if (!conn) {
+            return false;
+        }
+
+        conn->close(boost::system::error_code{});
+
+        return true;
+    }
+};
+
+int main(const int argc, const char* argv[])
+{
+    if (argc != 3) {
+        std::cerr << "You must specify the host and service or port for the client to connect to.\n";
+        return -1;
+    }
+
+    auto host = argv[1];
+    auto service_or_port = argv[2];
+
+    try {
+        auto chat_application = std::make_shared<chat_client>();
+        boost::asio::io_context io_context;
+        tcp_client<chat_client> client{io_context, chat_application, host, service_or_port};
+        std::thread t{[&io_context]() { io_context.run(); }};
+
+        std::string line;
+        while (std::getline(std::cin, line)) {
+            chat_application->send(line);
+        }
+
+        chat_application->close();
+
+        t.join();
+    }
+    catch (const std::exception& e) {
+        std::cerr << "Exception: " << e.what() << "\n";
+        return -1;
+    }
+
+    return 0;
+}
